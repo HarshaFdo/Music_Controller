c3b3fe264e856920ff4040ff661c079c3b4538b0
HarshaFdo <lhsfernando@std.appsc.sab.ac.lk>
2024-10-22 11:34:16 +0530

    d4

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,23 +1,25 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import HomePage from "./HomePage";
+
 
 export default class App extends Component {
   constructor(props) {
     super(props);
+
   }
 
   render() {
-<<<<<<< HEAD
-    return <h1>{this.props.name}</h1>;
-=======
-    return <h1>Testing React Code</h1>;
->>>>>>> 3aad44eaae9fce12ccda086af4a345b614a30b13
+    return (<div>
+      <HomePage />
+      <RoomJoinPage/>
+      <CreateRoomPage/>
+    </div>  
+    );  
   }
 }
 
 const appDiv = document.getElementById("app");
-<<<<<<< HEAD
-render(<App name = "tim"/>, appDiv);
-=======
-render(<App></App>, appDiv);
->>>>>>> 3aad44eaae9fce12ccda086af4a345b614a30b13
+
+render(<App />, appDiv);
+
